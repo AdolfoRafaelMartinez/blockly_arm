6456cd8cdfcf9dbe8f08a960da83dc8d0159e1d9
adolfo rafael martinez <adolfo.rafael.martinez@gmail.com>
2020-08-14 14:58:12 -0500

    my svg appears!

--- core/mutator.js
+++ core/mutator.js
@@ -190,7 +190,18 @@ Blockly.Mutator.prototype.createEditor_ = function() {
     // dragging work properly.
     background.insertBefore(flyoutSvg, this.workspace_.svgBlockCanvas_);
   }
-  this.svgDialog_.appendChild(background);
+
+  const svg1 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg1.setAttribute("width", "100");
+  svg1.setAttribute("height", "100");
+  const cir1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  cir1.setAttribute("cx", "80");
+  cir1.setAttribute("cy", "80");
+  cir1.setAttribute("r", "30");
+  cir1.setAttribute("fill", "red");
+  svg1.appendChild(cir1);
+  
+  this.svgDialog_.appendChild(svg1);
 
   return this.svgDialog_;
 };
